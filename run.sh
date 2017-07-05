2e2ab1c5298c96f6d88dec89b36aad237494d5e4
yann marquet <ymarquet@gmail.com>
2017-07-05 03:01:31 +0100

    try long options

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gem install wercker_api --no-document
+gem install wercker_api --no-rdoc --no-ri
 
 [ -z "$WERCKER_TRIGGER_PIPELINE_PIPELINE_ID" ] && { echo "Step stupidcodefactory/trigger-pipeline requires pipeline-id key to be set"; exit 1 }
 [ -z "$WERCKER_TRIGGER_PIPELINE_API_TOKEN" ] && { echo "Step stupidcodefactory/trigger-pipeline requires api-token key to be set"; exit 1 }

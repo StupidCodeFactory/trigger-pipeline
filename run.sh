e21a355869bc3696088efde4219e90a824209371
yann marquet <ymarquet@gmail.com>
2017-07-05 02:59:49 +0100

    fix gem install no doc

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gem install wercker_api --no-doc
+gem install wercker_api --no-document
 
 [ -z "$WERCKER_TRIGGER_PIPELINE_PIPELINE_ID" ] && { echo "Step stupidcodefactory/trigger-pipeline requires pipeline-id key to be set"; exit 1 }
 [ -z "$WERCKER_TRIGGER_PIPELINE_API_TOKEN" ] && { echo "Step stupidcodefactory/trigger-pipeline requires api-token key to be set"; exit 1 }

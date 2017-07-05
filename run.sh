6f41fc45d2248bee2860a6d024273fa589071b09
yann marquet <ymarquet@gmail.com>
2017-07-05 03:28:24 +0100

    relative patch

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ gem install wercker_api
 [ -z "$WERCKER_TRIGGER_PIPELINE_PIPELINE_ID" ] && { echo "Step stupidcodefactory/trigger-pipeline requires pipeline-id key to be set"; exit 1;}
 [ -z "$WERCKER_TRIGGER_PIPELINE_API_TOKEN" ] && { echo "Step stupidcodefactory/trigger-pipeline requires api-token key to be set"; exit 1;}
 
-ruby main.rb
+ruby ./main.rb

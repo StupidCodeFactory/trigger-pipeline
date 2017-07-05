382ff68b67a753e864f610a8741fa69159f7c85d
yann marquet <ymarquet@gmail.com>
2017-07-05 03:42:29 +0100

    Bump

--- run.sh
+++ run.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-gem install wercker_api
+gem install wercker_api --no-doc --quiet
 
 [ -z "$WERCKER_TRIGGER_PIPELINE_PIPELINE_ID" ] && { echo "Step stupidcodefactory/trigger-pipeline requires pipeline-id key to be set"; exit 1;}
 [ -z "$WERCKER_TRIGGER_PIPELINE_API_TOKEN" ] && { echo "Step stupidcodefactory/trigger-pipeline requires api-token key to be set"; exit 1;}
-pwd
-ls -la
+
 ruby "$WERCKER_STEP_ROOT/main.rb"

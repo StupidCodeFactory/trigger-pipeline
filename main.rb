60fa492888f07ac500572947dd8f09a47c82877f
yann marquet <ymarquet@gmail.com>
2017-07-05 02:36:37 +0100

    add run.sh and wercker-step.yml

---
+++ main.rb
@@ -0,0 +1,19 @@
+require 'wercker_api'
+
+client      =  WerckerAPI::Client.new(ENV['WERCKER_TRIGGER_PIPELINE_API_TOKEN'])
+max_attemps = Integer(ENV.fetch('WERCKER_TRIGGER_PIPELINE_MAX_ATTEMPTS')) rescue nil
+delay       = Integer(ENV.fetch('WERCKER_TRIGGER_PIPELINE_DELAY'))        rescue nil
+pipeline_id = ENV['WERCKER_TRIGGER_PIPELINE_PIPELINE_ID']
+
+runner = WerckerAPI::PipelineRunner.new(
+  client, max_attemps: max_attemps || 180, delay: delay || 5
+)
+
+runner.run(pipeline_id)
+
+unless run.status == 'passed'
+  STDOUT.puts "Pipeline #{pipeline_id} failed. Aborting build."
+  exit 1
+end
+
+exit 0
---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+gem install wercker_api --no-doc
+
+[ -z "$WERCKER_TRIGGER_PIPELINE_PIPELINE_ID" ] && { echo "Step stupidcodefactory/trigger-pipeline requires pipeline-id key to be set"; exit 1 }
+[ -z "$WERCKER_TRIGGER_PIPELINE_API_TOKEN" ] && { echo "Step stupidcodefactory/trigger-pipeline requires api-token key to be set"; exit 1 }
+
+ruby main.rb

95c9eda8c7ca15f3432bba786203b69453dc2337
yann marquet <ymarquet@gmail.com>
2017-07-05 03:39:13 +0100

    Fix and bump

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ gem install wercker_api
 [ -z "$WERCKER_TRIGGER_PIPELINE_API_TOKEN" ] && { echo "Step stupidcodefactory/trigger-pipeline requires api-token key to be set"; exit 1;}
 pwd
 ls -la
-ruby "$WERCKER_STEP_ROOT./main.rb"
+ruby "$WERCKER_STEP_ROOT/main.rb"
